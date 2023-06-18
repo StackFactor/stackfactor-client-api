0ed9db419e4d89daa77ca27488b5efb3ab1896c1
CANDIN JONES <candin@gmail.com>
2023-06-18 01:25:10 -0400

    Changed the exports

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-export * from "./lib/actionNotifications";
-export * from "./lib/address";
-export * from "./lib/config";
-export * from "./lib/integration";
-export * from "./lib/integrationsConfiguration";
-export * from "./lib/departmentTraiingPlans";
-export * from "./lib/groups";
-export * from "./lib/role";
-export * from "./lib/roleTemplate";
-export * from "./lib/skill";
-export * from "./lib/skillAssessments";
-export * from "./lib/skillTemplate";
-export * from "./lib/teams";
-export * from "./lib/templateDocument";
-export * from "./lib/templatePhase";
-export * from "./lib/templateRelease";
-export * from "./lib/templates";
-export * from "./lib/tenants";
-export * from "./lib/trainingPlanTemplate";
-export * from "./lib/trainingPlans";
-export * from "./lib/userInformation";
-export * from "./lib/users";
+const actionNotifications = require("./lib/actionNotifications");
+const address = require("./lib/address");
+const config = require("./lib/config");
+const integration = require("./lib/integration");
+const integrationsConfiguration = require("./lib/integrationsConfiguration");
+const departmentTraiingPlans = require("./lib/departmentTraiingPlans");
+const groups = require("./lib/groups");
+const role = require("./lib/role");
+const roleTemplate = require("./lib/roleTemplate");
+const skill = require("./lib/skill");
+const skillAssessments = require("./lib/skillAssessments");
+const skillTemplate = require("./lib/skillTemplate");
+const teams = require("./lib/teams");
+const templateDocument = require("./lib/templateDocument");
+const templatePhase = require("./lib/templatePhase");
+const templateRelease = require("./lib/templateRelease");
+const templates = require("./lib/templates");
+const tenants = require("./lib/tenants");
+const trainingPlanTemplate = require("./lib/trainingPlanTemplate");
+const trainingPlans = require("./lib/trainingPlans");
+const userInformation = require("./lib/userInformation");
+const users = require("./lib/users");
+
+module.exports = {
+  ...actionNotifications,
+  ...address,
+  ...config,
+  ...integration,
+  ...integrationsConfiguration,
+  ...departmentTraiingPlans,
+  ...groups,
+  ...role,
+  ...roleTemplate,
+  ...skill,
+  ...skillAssessments,
+  ...skillTemplate,
+  ...teams,
+  ...templateDocument,
+  ...templatePhase,
+  ...templateRelease,
+  ...templates,
+  ...tenants,
+  ...trainingPlanTemplate,
+  ...trainingPlans,
+  ...userInformation,
+  ...users,
+};
