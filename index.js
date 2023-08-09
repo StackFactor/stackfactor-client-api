8eaeea44f6dd4c794d4e7dbf58acb624ee2667cb
Candin Jones <candin@gmail.com>
2023-08-08 21:48:20 -0400

    STAC-791 Code changes

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const actionNotifications = require("./lib/actionNotifications");
+const axios = require("./lib/axios");
 const address = require("./lib/address");
 const config = require("./lib/config");
 const constants = require("./lib/constants");
 const dashboard = require("./lib/dashboard");
 const departmentTraingPlans = require("./lib/departmentTrainingPlans");
 const integration = require("./lib/integration");
-const integrationsConfiguration = require("./lib/integrationConfiguration");
+const integrationConfiguration = require("./lib/integrationConfiguration");
 const groups = require("./lib/groups");
 const learningContent = require("./lib/learningContent");
+const logger = require("./lib/logger");
 const role = require("./lib/role");
 const roleTemplate = require("./lib/roleTemplate");
 const skill = require("./lib/skill");
-const skillAssessments = require("./lib/skillAssessments");
+const skillAssessment = require("./lib/skillAssessments");
+const skillAssessmentTestingSession = require("./lib/skillAssessmentTestingSession");
 const skillTemplate = require("./lib/skillTemplate");
-const teams = require("./lib/teams");
-const templateDocument = require("./lib/templateDocument");
-const templatePhase = require("./lib/templatePhase");
-const templateRelease = require("./lib/templateRelease");
+const team = require("./lib/teams");
+// const templateDocument = require("./lib/templateDocument");
+// const templatePhase = require("./lib/templatePhase");
+// const templateRelease = require("./lib/templateRelease");
 const templates = require("./lib/templates");
 const tenants = require("./lib/tenants");
 const trainingPlanTemplate = require("./lib/trainingPlanTemplate");
-const trainingPlans = require("./lib/trainingPlans");
+const trainingPlan = require("./lib/trainingPlans");
 const userInformation = require("./lib/userInformation");
 const users = require("./lib/users");
 
@@ -29,26 +32,32 @@ module.exports = {
   address,
   config,
   dashboard,
+  errorToString: axios.errorToString,
+  getErrorType: axios.getErrorType,
   DOCUMENT_VERSION: constants.DOCUMENT_VERSION,
   integration,
-  integrationsConfiguration,
+  integrationConfiguration,
   departmentTraingPlans,
   groups,
   learningContent,
+  logger,
   RESONSE_TYPE: constants.RESONSE_TYPE,
+  responseType: axios.responseType,
   role,
   roleTemplate,
+  shouldReturnError: axios.shouldReturnError,
   skill,
-  skillAssessments,
+  skillAssessment,
+  skillAssessmentTestingSession,
   skillTemplate,
-  teams,
-  templateDocument,
-  templatePhase,
-  templateRelease,
+  team,
+  // templateDocument,
+  // templatePhase,
+  // templateRelease,
   templates,
   tenants,
   trainingPlanTemplate,
-  trainingPlans,
+  trainingPlan,
   userInformation,
   users,
 };
