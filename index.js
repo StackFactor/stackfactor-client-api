7e36dae38e0b6cb3b505e3dd8c56f447330dce1e
Candin Jones <candin@gmail.com>
2023-08-09 10:29:08 -0400

    STAC-804 Code changes

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-import actionNotifications from "./lib/actionNotifications";
+import actionNotifications from "./client-api/lib/actionNotifications";
 import axios, {
   errorToString,
   getErrorType,
   shouldReturnError,
-} from "./lib/axiosClient";
-import address from "./lib/address";
-import config from "./lib/config";
-import constants from "./lib/constants";
-import contentGenerator from "./lib/integrations/contentGenerator";
-import dashboard from "./lib/dashboard";
-import departmentTraingPlans from "./lib/departmentTrainingPlans";
-import integration from "./lib/integration";
-import integrationConfiguration from "./lib/integrationConfiguration";
-import groups from "./lib/groups";
-import learningContent from "./lib/learningContent";
-import logger from "./lib/logger";
-import role from "./lib/role";
-import roleTemplate from "./lib/roleTemplate";
-import skill from "./lib/skill";
-import skillAssessment from "./lib/skillAssessments";
-import skillAssessmentTestingSession from "./lib/skillAssessmentTestingSession";
-import skillTemplate from "./lib/skillTemplate";
-import team from "./lib/teams";
-import tenant from "./lib/tenants";
-import trainingPlanTemplate from "./lib/trainingPlanTemplate";
-import trainingPlan from "./lib/trainingPlans";
-import userInformation from "./lib/userInformation";
-import users from "./lib/users";
+} from "./client-api/lib/axiosClient";
+import address from "./client-api/lib/address";
+import config from "./client-api/lib/config";
+import constants from "./client-api/lib/constants";
+import contentGenerator from "./client-api/lib/integrations/contentGenerator";
+import dashboard from "./client-api/lib/dashboard";
+import departmentTraingPlans from "./client-api/lib/departmentTrainingPlans";
+import integration from "./client-api/lib/integration";
+import integrationConfiguration from "./client-api/lib/integrationConfiguration";
+import groups from "./client-api/lib/groups";
+import learningContent from "./client-api/lib/learningContent";
+import logger from "./client-api/lib/logger";
+import role from "./client-api/lib/role";
+import roleTemplate from "./client-api/lib/roleTemplate";
+import skill from "./client-api/lib/skill";
+import skillAssessment from "./client-api/lib/skillAssessments";
+import skillAssessmentTestingSession from "./client-api/lib/skillAssessmentTestingSession";
+import skillTemplate from "./client-api/lib/skillTemplate";
+import team from "./client-api/lib/teams";
+import tenant from "./client-api/lib/tenants";
+import trainingPlanTemplate from "./client-api/lib/trainingPlanTemplate";
+import trainingPlan from "./client-api/lib/trainingPlans";
+import userInformation from "./client-api/lib/userInformation";
+import users from "./client-api/lib/users";
 
 export default {
   actionNotifications: actionNotifications,
