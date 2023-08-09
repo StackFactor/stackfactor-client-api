16a730657b03ce3f1ca14d9522b9e9fc25903e46
Candin Jones <candin@gmail.com>
2023-08-09 16:25:08 -0400

    STAC-804 Code changes

--- index.js
+++ index.js
@@ -1,61 +1,63 @@
 import actionNotifications from "./lib/actionNotifications.js";
 export { actionNotifications };
-import axios, {
+import {
+  client,
   errorToString,
   getErrorType,
   shouldReturnError,
 } from "./lib/axiosClient.js";
-export * from "./lib/axiosClient.js";
+export { client, errorToString, getErrorType, shouldReturnError };
 import address from "./lib/address.js";
-export * from "./lib/address.js";
+export { address };
 import config from "./lib/config.js";
-export * from "./lib/config.js";
+export { config };
 import constants from "./lib/constants.js";
+export { constants };
 import contentGenerator from "./lib/integrations/contentGenerator.js";
-export * from "./lib/integrations/contentGenerator.js";
+export { contentGenerator };
 import dashboard from "./lib/dashboard.js";
-export * from "./lib/dashboard.js";
+export { dashboard };
 import departmentTraingPlans from "./lib/departmentTrainingPlans.js";
-export * from "./lib/departmentTrainingPlans.js";
+export { departmentTraingPlans };
 import integration from "./lib/integration.js";
-export * from "./lib/integration.js";
+export { integration };
 import integrationConfiguration from "./lib/integrationConfiguration.js";
-export * from "./lib/integrationConfiguration.js";
+export { integrationConfiguration };
 import groups from "./lib/groups.js";
-export * from "./lib/groups.js";
+export { groups };
 import learningContent from "./lib/learningContent.js";
-export * from "./lib/learningContent.js";
+export { learningContent };
 import logger from "./lib/logger.js";
-export * from "./lib/logger.js";
+export { logger };
 import role from "./lib/role.js";
-export * from "./lib/role.js";
+export { role };
 import roleTemplate from "./lib/roleTemplate.js";
-export * from "./lib/roleTemplate.js";
+export { roleTemplate };
 import skill from "./lib/skill.js";
-export * from "./lib/skill.js";
+export { skill };
 import skillAssessment from "./lib/skillAssessments.js";
-export * from "./lib/skillAssessments.js";
+export { skillAssessment };
 import skillAssessmentTestingSession from "./lib/skillAssessmentTestingSession.js";
-export * from "./lib/skillAssessmentTestingSession.js";
+export { skillAssessmentTestingSession };
 import skillTemplate from "./lib/skillTemplate.js";
-export * from "./lib/skillTemplate.js";
+export { skillTemplate };
 import team from "./lib/teams.js";
-export * from "./lib/teams.js";
+export { team };
 import tenant from "./lib/tenants.js";
-export * from "./lib/tenants.js";
+export { tenant };
 import trainingPlanTemplate from "./lib/trainingPlanTemplate.js";
-export * from "./lib/trainingPlanTemplate.js";
+export { trainingPlanTemplate };
 import trainingPlan from "./lib/trainingPlans.js";
-export * from "./lib/trainingPlans.js";
+export { trainingPlan };
 import userInformation from "./lib/userInformation.js";
-export * from "./lib/userInformation.js";
+export { userInformation };
 import users from "./lib/users.js";
-export * from "./lib/users.js";
+export { users };
 
 export default {
   actionNotifications: actionNotifications,
   address: address,
-  axios: axios,
+  axios: client,
   config: config,
   contentGenerator: contentGenerator,
   dashboard: dashboard,
